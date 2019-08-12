54a91b60c8dad9e4439e58f1f2bb953035221363
Geoff Johnson <geoff.johnson@coanda.ca>
2019-08-11 23:50:29 -0700

    fix: change test configuration

--- src/test/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
+++ src/test/java/edu/udacity/java/nano/WebSocketChatApplicationTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WebMvcTest
 public class WebSocketChatApplicationTest {
 
