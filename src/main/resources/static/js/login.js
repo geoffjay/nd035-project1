4ef094e3a49370bf6e1e9aac0e38ac5c3e157560
Geoff Johnson <geoff.johnson@coanda.ca>
2019-08-11 20:40:34 -0700

    fix: handle key press correctly

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -6,14 +6,18 @@
 function login() {
     var username = document.getElementById("username").value.trim();
     var host = document.location.host;
-    var url = "http://" + host + "/index?username=" + username;
-    $(location).attr("href", url);
+    var url = "/index?username=" + username;
+    document.location.href = url;
 }
 
 /**
  * Enter to login.
  */
-document.onkeydown = function (event) {
-    var e = event || window.event || arguments.callee.caller.arguments[0];
-    e.keyCode === 13 && login(username);
-};
+$(document).ready(function() {
+    $(document).keypress(function(e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            login();
+        }
+    })
+})
